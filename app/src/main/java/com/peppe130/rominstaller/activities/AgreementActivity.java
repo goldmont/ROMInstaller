e7b07f2b44fccad10e84bab3f09d624fa01a00a7
Giuseppe Montuoro <peppe130@hotmail.it>
2016-08-09 11:02:22 +0200

    Update AgreementActivity.java

--- app/src/main/java/com/peppe130/rominstaller/activities/AgreementActivity.java
+++ app/src/main/java/com/peppe130/rominstaller/activities/AgreementActivity.java
@@ -1,6 +1,6 @@
 /*
     
-    Copyright 2016, Giuseppe Montuoro.
+    Copyright © 2016, Giuseppe Montuoro.
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
